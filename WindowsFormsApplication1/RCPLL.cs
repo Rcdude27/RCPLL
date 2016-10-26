db7387ba73c85165144dcdacf3d20f90469c6726
Rcdude27 <Rcdude27@DESKTOP-BJJPA97>
2016-10-26 12:44:25 -0500

    Initial

--- WindowsFormsApplication1/RCPLL.cs
+++ WindowsFormsApplication1/RCPLL.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using DataModels;
-using RubikCubePLLHelp;
+using RubikCubePLLHelp;  
 
 namespace RubiksCubePLLHelp
 {
